45768296b45e28f60d1d357fd36aaf18ca2dfb3c
Kenni Bawden <kenni.bawden@gmail.com>
2013-05-04 21:08:08 +1000

    Full screen map with google maps and angular ui

---
+++ busgo.js
@@ -0,0 +1,17 @@
+angular.module('busgo', ['ui.map'])
+       .value('uiMapConfig', {});
+
+function stopCtrl($scope) {
+	$scope.stop = {
+		id: 200039,
+		name: "Central Station, Eddy Av",
+		lat: -33.88250732421875,
+		lon: 151.2073974609375
+	};
+	
+	$scope.mapOptions = {
+		zoom: 16,
+		center: new google.maps.LatLng($scope.stop.lat, $scope.stop.lon),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+}
