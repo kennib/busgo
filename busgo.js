47aedbf3d6b64515ac87c33a13ae34ec6ff9ef71
unknown <Kenni@Kenni-PC.(none)>
2013-05-07 19:44:07 +1000

    Retrieving bus stop data from Parse and placing markers

--- busgo.js
+++ busgo.js
@@ -1,7 +1,28 @@
-angular.module('busgo', ['ui.map'])
-       .value('uiMapConfig', {});
+var app = angular.module('busgo', ['ui.map', 'Parse'])
+// Enable maps API
+app.value('uiMapConfig', {});
+// Enable Parse API
+app.config(function (ParseProvider) {
+	ParseProvider.initialize("XdeNxntxTKqBv0QvjsZfWdhKxmy74wMeqM4M42p5", "axK8ik0ppHi1smnjsQtTx04buL0M0FNKNKhb7vky");
+});
+app.run(function(Parse) {
+  return Parse.auth.resumeSession();
+});
+// The bus stop object
+app.factory('Stop', function(Parse) {
+	var Stop;
+	return Stop = (function(_super) {
+		__extends(Stop, _super);
+		function Stop() {
+			return Stop.__super__.constructor.apply(this, arguments);
+		}
+		// Stop Parse class name and attributes
+		Stop.configure('stops', 'stop_code', 'stop_id', 'stop_lat', 'stop_lon', 'stop_name');
+		return Stop;
+	})(Parse.Model);
+});
 
-function stopCtrl($scope) {
+function stopCtrl($scope, Stop) {
 	$scope.stop = {
 		id: 200039,
 		name: "Central Station, Eddy Av",
@@ -16,4 +37,29 @@ function stopCtrl($scope) {
 	};
 	
 	enableGestures($scope);
+	
+	// Get the user's location
+	navigator.geolocation.getCurrentPosition(function(position) {
+		$scope.location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	});
+	
+	// Get Bus Stop data
+	$scope.stops = [];
+	$scope.stopMarkers = [];
+	Stop.query({limit: 999999}).then(function(stops) {
+		for (var s in stops) {
+			// Get the stop object
+			var stop = stops[s];
+			
+			// Create a google maps marker
+			var coord = new google.maps.LatLng(stop.stop_lat, stop.stop_lon);
+			var marker = new google.maps.Marker({
+				map: $scope.map,
+				position: coord
+			});
+			
+			// Add it to the list of markers
+			$scope.stopMarkers.push(marker);
+		}
+	});
 }
