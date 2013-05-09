acb068c8712b50178e6d4e8f63b9da399b6a2c34
unknown <Kenni@Kenni-PC.(none)>
2013-05-10 06:54:28 +1000

    Function for mapping routes

--- busgo.js
+++ busgo.js
@@ -21,8 +21,68 @@ app.factory('Stop', function(Parse) {
 		return Stop;
 	})(Parse.Model);
 });
+// The trip object
+app.factory('Trip', function(Parse) {
+	var Trip;
+	return Trip = (function(_super) {
+		__extends(Trip, _super);
+		function Trip() {
+			return Trip.__super__.constructor.apply(this, arguments);
+		}
+		// Trip Parse class name and attributes
+		Trip.configure('trips', 'direction_id', 'route_id', 'shape_id', 'trip_headsign', 'trip_id', 'wheelchair_accessible');
+		return Trip;
+	})(Parse.Model);
+});
+// The shape object
+app.factory('Shape', function(Parse) {
+	var Shape;
+	return Shape = (function(_super) {
+		__extends(Shape, _super);
+		function Shape() {
+			return Shape.__super__.constructor.apply(this, arguments);
+		}
+		// Shape Parse class name and attributes
+		Shape.configure('shapes', 'shape_id', 'shape_pt_lat', 'shape_pt_lng', 'shape_pt_sequence');
+		return Shape;
+	})(Parse.Model);
+});
+// Map a route
+app.factory('mapRoute', function(Trip, Shape) {
+	return function(route_id, scope) {
+		// Find the trip
+		Trip.query({
+			where: {
+				route_id: route_id
+			},
+			limit: 1
+		}).then(function(trips) {
+			var trip = trips[0]
+			// Get the route's shape
+			Shape.query({
+				where: {
+					shape_id: trip.shape_id
+				}
+			}).then(function(shape_points) {
+				// Get the points into an array
+				var points = [];
+				for (var p in shape_points) {
+					var point = shape_points[p];
+					var latlng = new google.maps.LatLng(point.shape_pt_lat, point.shape_pt_lon);
+					points[point.shape_pt_sequence] = latlng;
+				}
+				// Create the line
+				new google.maps.Polyline({
+					map: scope.map,
+					path: points 
+				});
+			});
+		});
+	};
+});
 
-function stopCtrl($scope, Stop) {
+
+function stopCtrl($scope, Stop, mapRoute) {
 	$scope.stop = {
 		id: 200039,
 		name: "Central Station, Eddy Av",
@@ -70,7 +130,6 @@ function stopCtrl($scope, Stop) {
 	$scope.stops = [];
 	$scope.stopMarkers = [];
 	Stop.query({limit: 999999}).then(function(stops) {
-		console.log(stops.length, "Stops");
 		for (var s in stops) {
 			// Get the stop object
 			var stop = stops[s];
@@ -86,4 +145,6 @@ function stopCtrl($scope, Stop) {
 			$scope.stopMarkers.push(marker);
 		}
 	});
+	
+	mapRoute("11954_X94", $scope);
 }
