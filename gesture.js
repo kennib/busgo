2f3e90c3950e744aabf5bf07106faca792f1b996
unknown <Kenni@Kenni-PC.(none)>
2013-05-10 08:29:52 +1000

    Use the kinect to move the map

--- gesture.js
+++ gesture.js
@@ -25,21 +25,43 @@ function enableGestures(scope) {
 		console.log('Session ended')
 	});
 	
-	// SwipeDetector
-	var swipeDetector = zig.controls.SwipeDetector();
-	swipeDetector.addEventListener('swipe', function(dir) {
-		console.log('SwipeDetector: Swipe direction: ' + dir);
-		var delta = 0.005;
-		var pos = scope.map.getCenter();
-		var lat = pos.lat();
-		var lng = pos.lng();
-		
-		if (dir == "left")  lng -= delta;
-		if (dir == "right") lng += delta;
-		if (dir == "up")    lat += delta;
-		if (dir == "down")  lat -= delta;
-		
-		scope.map.panTo(new google.maps.LatLng(lat, lng));
+	// Create cursor and cursor dom element
+	var c = zig.controls.Cursor();
+	var ce = document.createElement('div');
+	ce.id = 'mycursor';
+	document.body.appendChild(ce);
+	 
+	// 1. show/hide cursor on session start/end
+	zig.singleUserSession.addEventListener('sessionstart', function(focusPosition) {
+		ce.style.display = 'block';
 	});
-	zig.singleUserSession.addListener(swipeDetector);
+	zig.singleUserSession.addEventListener('sessionend', function() {
+		ce.style.display = 'none';
+	});
+	 
+	// 2. move the cursor element on cursor move
+	c.addEventListener('move', function(cursor) {
+		ce.style.left = (c.x * window.innerWidth - (ce.offsetWidth / 2)) + "px";
+		ce.style.top = (c.y * window.innerHeight - (ce.offsetHeight / 2)) + "px";
+		
+		
+		// Move the map if the cursor is on the edge of the screen
+		var delta = 0.0004;
+		var threshold = 0.1;
+		var latlng = scope.map.getCenter();
+		var lat = latlng.lat();
+		var lng = latlng.lng();
+		
+		if (c.x < threshold)     lng -= delta;
+		if (1.0-c.x < threshold) lng += delta;
+		if (c.y < threshold)     lat += delta;
+		if (1.0-c.y < threshold) lat -= delta;
+		
+		latlng = new google.maps.LatLng(lat, lng);
+		
+		scope.map.panTo(latlng);
+	});
+	 
+	// Add cursor to our single user UI session
+	zig.singleUserSession.addListener(c);
 }
