3b567112b16fe1dd98b6fdf33ab0cf1b9ae7ad33
kennib <kenni.bawden@gmail.com>
2013-05-20 12:26:25 +1000

    Stops on the buses page can be accessed via url

--- buses.js
+++ buses.js
@@ -1,3 +1,16 @@
-function busesCtrl($scope) {
-
+function busesCtrl($scope, $routeParams,
+                   Stop) {
+	// Get the stop
+	var stopId = $routeParams.stopId;
+	Stop.query({
+		where: JSON.stringify({
+			stop_id: stopId
+		})
+	}).then(function(stops) {
+		if (stops.length > 0) {
+			$scope.stop = stops[0];
+		} else {
+			$scope.stop = {name: "Invalid stop"};
+		}
+	});
 }
