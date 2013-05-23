d7d7bc8a96f8cffae8a83ff2f36434705748fe31
= <=>
2013-05-23 23:36:17 +1000

    Fixed header links

--- places.js
+++ places.js
@@ -1,4 +1,4 @@
-function placesCtrl($scope, $routeParams, $timeout) {
+function placesCtrl($scope, $routeParams, $timeout, Stop) {
 	// Page attributes
 	$scope.title = "Places";
 	$scope.leftLink = "map";
@@ -11,11 +11,15 @@ function placesCtrl($scope, $routeParams, $timeout) {
 	$scope.validTypes = ['accounting','airport','amusement_park','aquarium','art_gallery','atm','bakery','bank','bar','beauty_salon','bicycle_store','book_store','bowling_alley','bus_station','cafe','campground','car_dealer','car_rental','car_repair','car_wash','casino','cemetery','church','city_hall','clothing_store','convenience_store','courthouse','dentist','department_store','doctor','electrician','electronics_store','embassy','establishment','finance','fire_station','florist','food','funeral_home','furniture_store','gas_station','general_contractor','grocery_or_supermarket','gym','hair_care','hardware_store','health','hindu_temple','home_goods_store','hospital','insurance_agency','jewelry_store','laundry','lawyer','library','liquor_store','local_government_office','locksmith','lodging','meal_delivery','meal_takeaway','mosque','movie_rental','movie_theater','moving_company','museum','night_club','painter','park','parking','pet_store','pharmacy','physiotherapist','place_of_worship','plumber','police','post_office','real_estate_agency','restaurant','roofing_contractor','rv_park','school','shoe_store','shopping_mall','spa','stadium','storage','store','subway_station','synagogue','taxi_stand','train_station','travel_agency','university','veterinary_care','zoo'];
 	
 	// Get the user's location
-	if ($routeParams.stop) {
+	if ($routeParams.stopId) {
+		// Update links
+		$scope.leftLink = "map?stop="+$routeParams.stopId;
+		$scope.rightLink = "buses/stop/"+$routeParams.stopId;
+		
 		// Use URL route parameter to find a stop
 		Stop.query({
 			where: JSON.stringify({
-				stop_id: $routeParams.stop
+				stop_id: $routeParams.stopId
 			})
 		}).then(function(stops) {
 			if (stops.length > 0) {
